d5a5eb50a8a183ee9dc905ea2f4d61b1b8fc1a90
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-06-25 16:55:52 +0100

    first commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'http://localhost:5173/ton-connect.manifest.json';
+const manifestUrl = 'https://beadtest-ik5d.vercel.app/ton-connect.manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
